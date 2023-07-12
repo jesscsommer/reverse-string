de857c0df751d055ce10eb166d5c1617de0615c7
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-12 16:41:54 -0700

    Add JS solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,4 +1,12 @@
 function reverseString(str) {
+
+  const new_string = []
+
+  for (let i=0; i < str.length; i++) {
+      new_string[str.length-i] = str[i]
+  }
+
+  return new_string.join("")
   // type your code here
 }
 
@@ -17,3 +25,9 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// get length of the string 
+// go through the string, for each letter in the string --> add it to a new list
+// make its position at whatever the len of the string is minus its current position or index 
+// join the list and return the new string 
+
